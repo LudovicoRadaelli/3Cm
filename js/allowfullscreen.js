0b4a938fead045733565cbd4abdbb8df7a305f40
LudovicoRadaelli <69393267+LudovicoRadaelli@users.noreply.github.com>
2025-09-15 13:28:44 +0200

    Add files via upload

---
+++ js/allowfullscreen.js
@@ -0,0 +1,42 @@
+let startX = 0;
+
+function handleTouchStart(evt) {
+    startX = evt.touches[0].clientX;
+}
+
+function handleTouchMove(evt) {
+    if (!startX) return;
+    let deltaX = evt.touches[0].clientX - startX;
+    if (Math.abs(deltaX) > 50) { // soglia minima per considerare swipe
+        if (deltaX > 0) {
+            previousSlide(2);
+        } else {
+            nextSlide(2);
+        }
+        startX = 0; // reset
+    }
+}
+
+const container = document.getElementById("slide-container2");
+container.addEventListener("touchstart", handleTouchStart, false);
+container.addEventListener("touchmove", handleTouchMove, false);
+
+
+function toggleFullscreen(elementId) {
+    const elem = document.getElementById(elementId);
+    if (!document.fullscreenElement) {
+        if (elem.requestFullscreen) {
+            elem.requestFullscreen();
+        } else if (elem.webkitRequestFullscreen) {
+            /* Safari */
+            elem.webkitRequestFullscreen();
+        } else if (elem.msRequestFullscreen) {
+            /* IE11 */
+            elem.msRequestFullscreen();
+        }
+    } else {
+        if (document.exitFullscreen) {
+            document.exitFullscreen();
+        }
+    }
+}
