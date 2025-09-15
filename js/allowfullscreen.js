d709730a526fdd2fc2e1ec8ec372c8dca04c89e7
LudovicoRadaelli <69393267+LudovicoRadaelli@users.noreply.github.com>
2025-09-15 13:35:26 +0200

    Update allowfullscreen.js

--- js/allowfullscreen.js
+++ js/allowfullscreen.js
@@ -1,3 +1,21 @@
+function toggleFullscreen(elementId) {
+    const elem = document.getElementById(elementId);
+    if (!document.fullscreenElement) {
+        if (elem.requestFullscreen) {
+            elem.requestFullscreen();
+        } else if (elem.webkitRequestFullscreen) { /* Safari */
+            elem.webkitRequestFullscreen();
+        } else if (elem.msRequestFullscreen) { /* IE11 */
+            elem.msRequestFullscreen();
+        }
+    } else {
+        if (document.exitFullscreen) {
+            document.exitFullscreen();
+        }
+    }
+}
+
+
 let startX = 0;
 
 function handleTouchStart(evt) {
@@ -7,7 +25,7 @@ function handleTouchStart(evt) {
 function handleTouchMove(evt) {
     if (!startX) return;
     let deltaX = evt.touches[0].clientX - startX;
-    if (Math.abs(deltaX) > 50) { // soglia minima per considerare swipe
+    if (Math.abs(deltaX) > 50) {  // soglia minima per considerare swipe
         if (deltaX > 0) {
             previousSlide(2);
         } else {
@@ -22,21 +40,3 @@ container.addEventListener("touchstart", handleTouchStart, false);
 container.addEventListener("touchmove", handleTouchMove, false);
 
 
-function toggleFullscreen(elementId) {
-    const elem = document.getElementById(elementId);
-    if (!document.fullscreenElement) {
-        if (elem.requestFullscreen) {
-            elem.requestFullscreen();
-        } else if (elem.webkitRequestFullscreen) {
-            /* Safari */
-            elem.webkitRequestFullscreen();
-        } else if (elem.msRequestFullscreen) {
-            /* IE11 */
-            elem.msRequestFullscreen();
-        }
-    } else {
-        if (document.exitFullscreen) {
-            document.exitFullscreen();
-        }
-    }
-}
